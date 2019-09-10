86a111d72613f93da5f3b27b41d361f206bdf0f2
Dario Strbenac <dario.strbenac@sydney.edu.au>
2019-09-10 18:02:48 +1000

    Commented Out No Longer Used Code

--- bulk/runAllelePredictor.R
+++ bulk/runAllelePredictor.R
@@ -14,30 +14,32 @@ bowtie2IndexPrefix <- "/savona/nobackup/biostat/indexes/bowtie2/polymorphic/poly
 prepareAndIndexSequences(genesGFF3, hg38sequenceFile, STARmaskedFolder, bowtie2IndexPrefix, allAllelesFASTA, outputGTFfile,
                          readLength = 150)
 
-R1s <- list.files("/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF10479_C79UVANXX",
+## No longer done separately. Use RNAanalysis.R.
+
+# R1s <- list.files("/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF10479_C79UVANXX",
                   ".*R1.*trimmed.*gz", full.names = TRUE) 
-R2s <- list.files("/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF10479_C79UVANXX",
+# R2s <- list.files("/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF10479_C79UVANXX",
                   ".*R2.*trimmed.*gz", full.names = TRUE)
-sampleNames <- sapply(lapply(strsplit(basename(R1s), '_'), '[', 1:2), paste, collapse = '_')
-predictQuantifyPolymorphicAlleles(R1s, R2s, sampleNames = sampleNames, maskedGenomePath = "/savona/nobackup/biostat/indexes/STAR/hg38maskedWithGENCODE26/",
+# sampleNames <- sapply(lapply(strsplit(basename(R1s), '_'), '[', 1:2), paste, collapse = '_')
+# predictQuantifyPolymorphicAlleles(R1s, R2s, sampleNames = sampleNames, maskedGenomePath = "/savona/nobackup/biostat/indexes/STAR/hg38maskedWithGENCODE26/",
                           maskedGenomeName = "hg38masked", threads = 8, bowtie2polymorphicIndex = "polymorphic/polyUniq", predictionsFolder = "/savona/nobackup/dario/TestRun/"
                           )
 
-RNAfolders <- c("/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF10479_C79UVANXX",
-                "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF11240_C8768ANXX/",
-                "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF11395_C8768ANXX/",
-                "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF11830_C9CWMANXX/",
-                "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF13546_CAFANANXX/",
-                "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF14197_CAT5WANXX/",
-                "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF14665_CAUJPANXX/"
-                )
-
-invisible(lapply(RNAfolders, function(folder)
-{
-  R1s <- list.files(folder, ".*R1.*trimmed.*gz", full.names = TRUE) 
-  R2s <- list.files(folder, ".*R2.*trimmed.*gz", full.names = TRUE)
-  sampleNames <- sapply(lapply(strsplit(basename(R1s), '_'), '[', 1:2), paste, collapse = '_')
-  predictQuantifyPolymorphicAlleles(R1s, R2s, sampleNames = sampleNames, maskedGenomePath = "/savona/nobackup/biostat/indexes/STAR/hg38maskedWithGENCODE26/",
-                                    maskedGenomeName = "hg38masked", threads = 8, bowtie2polymorphicIndex = "polymorphic/polyUniq", predictionsFolder = folder
-                                    )
-}))
+# RNAfolders <- c("/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF10479_C79UVANXX",
+#                 "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF11240_C8768ANXX/",
+#                 "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF11395_C8768ANXX/",
+#                 "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF11830_C9CWMANXX/",
+#                 "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF13546_CAFANANXX/",
+#                 "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF14197_CAT5WANXX/",
+#                 "/savona/nobackup/biostat/datasets/HelenRizos/AGRF_CAGRF14665_CAUJPANXX/"
+#                 )
+# 
+# invisible(lapply(RNAfolders, function(folder)
+# {
+#   R1s <- list.files(folder, ".*R1.*trimmed.*gz", full.names = TRUE) 
+#   R2s <- list.files(folder, ".*R2.*trimmed.*gz", full.names = TRUE)
+#   sampleNames <- sapply(lapply(strsplit(basename(R1s), '_'), '[', 1:2), paste, collapse = '_')
+#   predictQuantifyPolymorphicAlleles(R1s, R2s, sampleNames = sampleNames, maskedGenomePath = "/savona/nobackup/biostat/indexes/STAR/hg38maskedWithGENCODE26/",
+#                                     maskedGenomeName = "hg38masked", threads = 8, bowtie2polymorphicIndex = "polymorphic/polyUniq", predictionsFolder = folder
+#                                     )
+# }))
